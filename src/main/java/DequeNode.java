0bf5fe58b82bddb325d6c6a641162ace9dfcfaa7
José Luis Bueno <74137433+jxtaaa@users.noreply.github.com>
2022-03-15 11:38:31 +0100

    New implementation of DequeNode.java

--- src/main/java/DequeNode.java
+++ src/main/java/DequeNode.java
@@ -1,15 +1,29 @@
 /**
- * Class representing a node of a double-ended queue (deque). Each node has pointers to
- * the next and previous nodes.
- * The previous and next of the first and last node of the deque is null.
+ * Class representing a node of a double-ended queue (deque). Each node has pointers to the next and
+ * previous nodes. The previous and next of the first and last node of the deque is null.
  *
  * @param <T>
  */
 public class DequeNode<T> {
-    private T item ;
-    private DequeNode<T> next ;
-    private DequeNode<T> previous ;
 
+    private T item;
+    private DequeNode<T> next;
+    private DequeNode<T> previous;
+
+    // Setters
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public void setNext(DequeNode<T> next) {
+        this.next = next;
+    }
+
+    public void setPrevious(DequeNode<T> previous) {
+        this.previous = previous;
+    }
+
+    // Getters
     public T getItem() {
         return item;
     }
@@ -22,29 +36,23 @@ public class DequeNode<T> {
         return previous;
     }
 
-    public void setNext(DequeNode<T> node){
-        this.next = node;
-    }
-
-    public void setPrevious(DequeNode<T> node){
-        this.previous = node;
-    }
-
     public DequeNode(T item, DequeNode<T> next, DequeNode<T> previous) {
-        this.item = item ;
-        this.next = next ;
-        this.previous = previous ;
+        this.item = item;
+        this.next = next;
+        this.previous = previous;
     }
 
     public boolean isFirstNode() {
-        return previous == null ;
+        return previous == null;
     }
 
     public boolean isLastNode() {
-        return next == null ;
+        return next == null;
     }
 
     public boolean isNotATerminalNode() {
-        return (!isFirstNode() && !isLastNode()) ;
+        return (!isFirstNode() && !isLastNode());
     }
 }
+
+
