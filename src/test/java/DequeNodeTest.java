a9439039851265f6b2a4e7d3c421e585ed40a431
NicoRomero-07 <nicoromero07@uma.es>
2022-03-15 12:21:07 +0100

    testCases.txt complete to DequeeNodeTest

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
-    // null <-- 2 --> 3
-    // 2 --> 3
+
     /*
     Siendo (item, next, previous)
 
